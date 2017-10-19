b5ce349d35fa03f2b0521900edc2f920c2678b16
Zach Dubuc <zach.dubuc@gmail.com>
2017-10-19 10:52:42 -0400

    Did stuff with narwhal. Mostly comments

--- ATaleOfTwoHorns/ATaleOfTwoHorns/Assets/Code/Player/Movement/MovementNarwhal.cs
+++ ATaleOfTwoHorns/ATaleOfTwoHorns/Assets/Code/Player/Movement/MovementNarwhal.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementNarwhale : MonoBehaviour
+public class MovementNarwhale : MovementBase
 {
+    /// <summary>
+    /// Made By Zach
+    /// This will handle the Narwhal movement.
+    /// Controls: Swim around by following the left thumbstick. Dash by hitting X (Temp)
+    /// </summary>
+    
 
 	// Use this for initialization
 	void Start ()
